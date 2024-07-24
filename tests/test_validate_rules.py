fb904b66e09eba42ef16529b68e4c55ece5d993f
emikeb <no@email.com>
2024-07-24 10:23:10 +0200

    Remove unused import

--- tests/test_validate_rules.py
+++ tests/test_validate_rules.py
@@ -1,34 +1,34 @@
-import pytest
 import copy
 import subprocess
+
 import yaml
 
-from retasc.validator.validate_rules import validate_rule_dict, validate_rule
-
+from retasc.validator.validate_rules import validate_rule, validate_rule_dict
 
 valid_rule_dict = {
     "name": "Example Rule",
     "prerequisites": {
         "pp_schedule_item_name": "Release Date",
         "days_before_or_after": 5,
-        "dependent_rules": ["Dependent Rule 1", "Dependent Rule 2"]
+        "dependent_rules": ["Dependent Rule 1", "Dependent Rule 2"],
     },
     "jira_issues": [
         {
             "template": "major_pre_beta/main.yaml",
             "subtasks": [
                 {"template": "major_pre_beta/subtasks/add_beta_repos.yaml"},
-                {"template": "major_pre_beta/subtasks/notify_team.yaml"}
-            ]
+                {"template": "major_pre_beta/subtasks/notify_team.yaml"},
+            ],
         },
-        {"template": "major_pre_beta/secondary.yaml"}
-    ]
+        {"template": "major_pre_beta/secondary.yaml"},
+    ],
 }
 
 invalid_rule_dict = copy.deepcopy(valid_rule_dict)
 invalid_rule_dict["prerequisites"]["days_before_or_after"] = "invalid_type"
 
 valid_rule_yaml = yaml.dump(valid_rule_dict, sort_keys=False)
+
 
 def test_validate_rule_dict_valid():
     assert validate_rule_dict(valid_rule_dict) is True
